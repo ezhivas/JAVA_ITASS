d3ca51ee1dda64732d16e44a2ce9eddabad011e9
Serhii Vasylets <akvatonk@gmail.com>
2020-11-03 18:33:38 +0200

    task AT-2 created

--- src/main/java/taskat2/Human.java
+++ src/main/java/taskat2/Human.java
@@ -1,4 +1,61 @@
 package taskat2;
 
 public class Human {
+    private String name;
+    private String eyeColor;
+    private int age;
+    private int height;
+
+    public Human(String name, String eyeColor, int age, int height) {
+        this.name = name;
+        this.eyeColor = eyeColor;
+        this.age = age;
+        this.height = height;
+    }
+
+    public Human() {
+        this.name = "Gregory";
+        this.eyeColor = "Dark";
+        this.age = 50;
+        this.height = 190;
+
+    }
+
+    public void greeting(){
+        System.out.println("My name`s " + name + "!" + "I`m " + age + " years old" + ".My eyes color  is " + eyeColor +
+                ".My height is " + height + ".");
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
--- src/main/java/taskat2/Main.java
+++ src/main/java/taskat2/Main.java
@@ -1,4 +1,22 @@
 package taskat2;
 
 public class Main {
+    public static void main(String[] args) {
+
+        Human human = new Human();
+        Men men = new Men("Sergey","brown", 38, 178, 17);
+        Women women = new Women("Natasha","gold", 33, 165, 50);
+        System.out.println("Instances for parent <Human> and extending it <Men> and <Women> was created.");
+        System.out.println("<Human> has a <greeting> method:");
+        human.greeting();
+        System.out.println("Method <Greeting> from parent class <Human> is available" +
+                " both for <Men> and <Women>");
+        men.greeting();
+        women.greeting();
+        System.out.println("Men has cash: " + men.getCash());
+        System.out.println("Women has " + women.getShoesAmount() + " pairs of shoes");
+        System.out.println("Women has own function <shopping>. Let`s call it.");
+        women.shopping();
+        System.out.println("<Human> hasn't <cash> from <Men>, <shoes> from <Women> and can`t <shopping> like a <Women>.");
+    }
 }

d677c4e3dcda43ac01f4d2815be04ab5014aba9f
Serhii Vasylets <akvatonk@gmail.com>
2020-11-12 10:03:28 +0200

    AT-9 calculator with logging and tests

--- src/main/java/task_at_7/Calculator.java
+++ src/main/java/task_at_7/Calculator.java
@@ -1,14 +1,11 @@
-package pack8;
+package task_at_7;
 
-import java.util.logging.*;
 import java.util.Scanner;
 
 public class Calculator {
 
-    private static final Logger logger = Logger.getLogger(Calculator.class.getName());
-
-
     Scanner scanner = new Scanner(System.in);
+
     public int mathOperationAdd(int value1, int value2) {
         return (value1 + value2);
     }
@@ -21,16 +18,21 @@ public class Calculator {
     public int mathOperationDivide(int value1, int value2) {
         int result;
         try {
-            result =  (value1 / value2);
+            result = (value1 / value2);
         } catch (ArithmeticException exception) {
             System.out.println("Division by Zero!");
+
             return 0;
         }
         return result;
     }
     public String inputFromConsole() {
-        return scanner.next();
-
+        try {
+            return scanner.next();
+        } catch (NumberFormatException exception) {
+            System.out.println("Wrong number format");
+        }
+        return null;
     }
 }
 
--- src/main/java/task_at_7/CalculatorRunner.java
+++ src/main/java/task_at_7/CalculatorRunner.java
@@ -1,6 +1,4 @@
-package pack8;
-
-import jdk.nashorn.internal.runtime.StoredScript;
+package task_at_7;
 
 import java.io.IOException;
 import java.util.logging.*;
@@ -21,8 +19,7 @@ public class CalculatorRunner {
         logger.config("Program starts right from here");
         Calculator calculator = new Calculator();
 
-        int value1, value2,levelValue;
-        String level="";
+        int value1, value2, levelValue;
 
         System.out.println("Enter the level of logging:\n" +
                 "1.Finer\n" +
@@ -56,19 +53,18 @@ public class CalculatorRunner {
 
         System.out.println("Enter the first number:");
         value1 = Integer.parseInt(calculator.inputFromConsole());
-        logger.info("First number entered");
-        logger.severe("Info Log");
-        logger.warning("Info Log");
-        logger.info("Info Log");
-        logger.finest("Really not important");
+        SomeLogger();
+
         System.out.println("Enter the first number:");
         value2 = Integer.parseInt(calculator.inputFromConsole());
+        logger.info("Second number");
+        SomeLogger();
 
         while (true) {
 
-            System.out.println("Enter the operation:\n"+
+            System.out.println("Enter the operation:\n" +
                     "1 - for Adding\n" +
-                    "2 - for Substraction\n" +
+                    "2 - for Subtraction\n" +
                     "3 - for Multiply\n" +
                     "4 - for Division\n" +
                     "0 - for Exit\n");
@@ -77,15 +73,24 @@ public class CalculatorRunner {
             if (operation == 1)
                 System.out.println("The result of adding is: " + calculator.mathOperationAdd(value1, value2) + "\n");
             else if (operation == 2)
-                System.out.println("The result of substraction is: " + calculator.mathOperationSubtraction(value1, value2));
+                System.out.println("The result of subtraction is: " + calculator.mathOperationSubtraction(value1, value2));
             else if (operation == 3)
                 System.out.println("The result of multiply is: " + calculator.mathOperationMultiply(value1, value2));
             else if (operation == 4)
                 System.out.println("The result of divide is: " + calculator.mathOperationDivide(value1, value2));
             else if (operation == 0) break;
         }
+        logger.info("Operation chosen");
+        SomeLogger();
 
 
+    }
 
+    static void SomeLogger() {
+
+        logger.severe("Severe");
+        logger.warning("Warning");
+        logger.finest("Really not important");
+        logger.config("Config");
     }
 }

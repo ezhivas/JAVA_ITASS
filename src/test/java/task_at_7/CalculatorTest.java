50a9c083e1655d172aa04d7758510ece81e62ba1
Serhii Vasylets <akvatonk@gmail.com>
2020-11-16 15:57:32 +0200

    AT-9 changes in test. One instance of calculator for all test

--- src/test/java/task_at_7/CalculatorTest.java
+++ src/test/java/task_at_7/CalculatorTest.java
@@ -1,39 +1,28 @@
 package task_at_7;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
 
 public class CalculatorTest {
-
+        Calculator calculator = new Calculator();
 
     @Test
     public void mathOperationAdd() {
-        Calculator calculator = new Calculator();
         Assert.assertEquals(10, calculator.mathOperationAdd(5, 5));
-
     }
-
     @Test
     public void mathOperationSubtraction() {
-        Calculator calculator = new Calculator();
         Assert.assertEquals(10, calculator.mathOperationSubtraction(20, 10));
     }
-
     @Test
     public void mathOperationMultiply() {
-        Calculator calculator = new Calculator();
-        Assert.assertEquals(10, calculator.mathOperationMultiply(2, 5));
+                Assert.assertEquals(10, calculator.mathOperationMultiply(2, 5));
     }
-
     @Test
     public void mathOperationDivide() {
-        Calculator calculator = new Calculator();
         Assert.assertEquals(10, calculator.mathOperationDivide(100, 10));
     }
-
     @Test
     public void mathOperationDivideByZero() {
-        Calculator calculator = new Calculator();
         Assert.assertEquals(0, calculator.mathOperationDivide(100, 0));
     }
 }
